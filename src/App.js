3b851faacbf89330de8a81bb42afe95676169ee8
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-05-23 10:41:32 +0200

    implemented training pair container and functionality

--- src/App.js
+++ src/App.js
@@ -12,13 +12,18 @@ import FetchStateContainer from './components/FetchStateContainer';
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [avgTradingPairPrice, setAvgTradingPairPrice] = useState(0);
+  const [avgTradingPairPrice, setAvgTradingPairPrice] = useState(null);
+  const [currencyPairButtonListData, setCurrencyPairButtonListData] =
+    useState(null);
 
-  // - feting the the price of currency pairs.
+  const [selectedPair, setSelectedPair] = useState(null);
+
   useEffect(() => {
     const fetchingCurrencyPairs = async () => {
       setIsLoading(true);
       try {
+        // - feting the the price of currency pairs.
+
         const bitmapCall = await axios(
           `https://www.bitstamp.net/api/v2/ticker/btcusd`
         );
@@ -31,8 +36,6 @@ function App() {
           `https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD` //, pos 7 = last price
         );
 
-        setIsLoading(false);
-
         const coinbase = parseInt(coinbaseCall.data.data.rates.USD);
         const bitmap = parseInt(bitmapCall.data.open);
         const bitfinex = parseInt(bitfinexCall.data[0][7]);
@@ -40,6 +43,16 @@ function App() {
         setAvgTradingPairPrice(
           ((coinbase + bitmap + bitfinex) / 3).toLocaleString()
         );
+
+        //-  getting currency pairs for button list
+
+        const currencyPairButtonList = await axios(
+          `https://www.bitstamp.net/api/v2/trading-pairs-info/`
+        );
+
+        setCurrencyPairButtonListData(currencyPairButtonList.data);
+
+        setIsLoading(false);
       } catch (err) {
         console.error(err);
         setIsLoading(false);
@@ -49,6 +62,8 @@ function App() {
 
     fetchingCurrencyPairs();
   }, []);
+
+  if (selectedPair) console.log(selectedPair);
 
   return (
     <div className=' flex flex-col h-screen '>
@@ -64,8 +79,11 @@ function App() {
         )}
         {!isLoading && !isError && (
           <>
-            <TradingPairBtnContainer />
-            <SelectedBtnContainer />
+            <TradingPairBtnContainer
+              currencyPairButtonListData={currencyPairButtonListData}
+              setSelectedPair={setSelectedPair}
+            />
+            <SelectedBtnContainer selectedPair={selectedPair} />
             <AverageTickerValues avgTradingPairPrice={avgTradingPairPrice} />
           </>
         )}
--- src/components/TradingPairBtnContainer.js
+++ src/components/TradingPairBtnContainer.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
-function TradingPairBtnContainer() {
+function TradingPairBtnContainer({
+  currencyPairButtonListData,
+  setSelectedPair,
+}) {
   return (
-    <div className='flex justify-center p-2 border border-blue-200 rounded-md shadow'>
-      <h2>Select your training pair</h2>
+    <div className='flex flex-col justify-center p-2 border border-blue-200 rounded-md shadow max-h-96  overflow-y-auto'>
+      <h2 className='p-2 text-center'>Select your training pair</h2>
+      <hr className='border-blue-100 border rounded-md ' />
 
-      <div></div>
+      <div className='grid grid-cols-3 gap-3 overflow-auto mt-2'>
+        {currencyPairButtonListData &&
+          currencyPairButtonListData.map((pair) => {
+            return (
+              <button
+                onClick={() =>
+                  setSelectedPair(pair.name.split('/').join('').toLowerCase())
+                }
+                className='p-2 border cursor-pointer border-gray-400 shadow rounded-md hover:bg-blue-400 hover:text-white s '>
+                {pair.name}
+              </button>
+            );
+          })}
+      </div>
     </div>
   );
 }
